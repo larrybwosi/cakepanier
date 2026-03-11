220a55adbbee9771af6382cf4d89f9ca2bc3d48e
larrybwosi <larrybwosi@gmail.com>
2026-03-11 11:40:03 +0300

    Logto init

---
+++ src/lib/logto.config.ts
@@ -0,0 +1,8 @@
+export const logtoConfig = {
+  endpoint: "https://2xujd2.logto.app/",
+  appId: "z3uivni1h3x3ivt3nf712",
+  appSecret: "0bZClDWBe0a1xZXFy2SE82Nkn0tyumC5",
+  baseUrl: "http://localhost:3000", // Change to your own base URL
+  cookieSecret: "uhtgeQD9WUshlj43DwYs7rk8AbikjmMI", // Auto-generated 32 digit secret
+  cookieSecure: process.env.NODE_ENV === "production",
+};
